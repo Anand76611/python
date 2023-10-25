f2c1acb151462ba2f6dc4ede809a7be169ae781b
Anand76611 <143251846+Anand76611@users.noreply.github.com>
2023-10-25 22:53:02 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,15 @@
+import pandas as pd;
+import numpy as np;
+import matplotlib.pyplot as plt;
+data=pd.read_csv("Iris - Iris.csv") ;
+print(data);
+
+data.plot(kind="scatter",
+          x='SepalLengthCm',
+          y='PetalLengthCm')
+plt.grid()
+data=data.dropna()
+print(data.head(50))
+
+y = pd.get_dummies(data.Species, prefix='Species')
+print(y.head(50))
